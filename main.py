682cde537f85c1fa9988fb16c15670dfd10f04c6
Davyd Braz <102470929+DavydBraz@users.noreply.github.com>
2022-12-04 16:35:37 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from Veiculo import Veiculo
+from Carro import Carro
+
+Carro1 = Carro('Conversivel', 'Honda', '2015', 32000)
+
+print(Carro1.get_Tipo(), Carro1.get_Marca(), Carro1.get_Ano(), Carro1.get_Preco())
